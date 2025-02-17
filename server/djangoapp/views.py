ce9c8215c0c5ca0c1259314c5532ad99ca299933
binalipatel96 <100009228+binalipatel96@users.noreply.github.com>
2025-02-16 21:14:20 -0500

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -26,7 +26,7 @@ def get_cars(request):
     count = CarMake.objects.filter().count()
     print(count)
     if (count == 0):
-    initiate()
+        initiate()
     car_models = CarModel.objects.select_related('car_make')
     cars = []
     for car_model in car_models:
